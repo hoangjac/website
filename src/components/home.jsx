74f048d32160f83d198d7dfe90c90f6dbcf31dbc
Jack Hoang <hoangjac@sheridancollege.ca>
2021-03-06 23:27:24 -0500

    changed picture and fixed some styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,16 +7,16 @@ export default class Home extends Component {
                 <section id="colorlib-hero" className="js-fullheight" data-section="home">
                     <div className="flexslider js-fullheight">
                         <ul className="slides">
-                            <li style={{ backgroundImage: 'url(images/banner.jpg)' }}>
+                            <li style={{ backgroundImage: 'url(images/IMG_0920.jpg)'}}>
                                 <div className="overlay" />
                                 <div className="container-fluid">
                                     <div className="row">
-                                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                                        <div className="col-md-6 col-md-offset-10 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                             <div className="slider-text-inner js-fullheight">
                                                 <div className="desc">
-                                                    <h1>Hi! <br />I'm Jack</h1>
-                                                    <h2>Take a look around to get to know me</h2>
-                                                        <p><a className="btn btn-primary btn-learn" style={{ color: "white" }}
+                                                    <h1 style={{color: "black", fontWeight: "bold"}}>Hi! <br />I'm Jack</h1>
+                                                    <h2 style={{color: "black", fontWeight: "bold"}}>Take a look around to get to know me</h2>
+                                                        <p><a className="btn btn-primary btn-learn" style={{ color: "black", fontWeight: "bold" }}
                                                             href="https://drive.google.com/file/d/1qPjNGxyTdqOpAKEFa4YEBvWv7uDl5G0I/view?usp=sharing"
                                                             target="_blank" rel="noopener noreferrer"> Download CV <i className="icon-download4" /></a></p>
                                                 </div>
@@ -25,17 +25,17 @@ export default class Home extends Component {
                                         </div>
                                     </div>
                             </li>
-                                <li style={{ backgroundImage: 'url(images/banner.jpg)' }}>
+                                <li style={{ backgroundImage: 'url(images/IMG_0920.jpg)' }}>
                                     <div className="overlay"></div>
                                     <div className="container-fluid">
                                         <div className="row">
-                                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                                            <div className="col-md-3 col-md-offset-10 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                                                 <div className="slider-text-inner">
                                                     <div className="desc">
-                                                        <h1>I am <br />a Software Development Student</h1>
-                                                        <h2>View some of my work below!</h2>
+                                                        <h1 style={{color: "black", fontWeight: "bold"}}>I am <br />a Software Development Student</h1>
+                                                        <h2 style={{color: "black", fontWeight: "bold"}}>View some of my work below!</h2>
                                                         <nav id="colorlib-main-menu" role="navigation" className="navbar">
-                                                            <p><a href="https://github.com/hoangjac" target="_blank" style={{ color: "white" }} rel="noopener noreferrer"
+                                                            <p><a href="https://github.com/hoangjac" target="_blank" style={{ color: "black", fontWeight: "bold" }} rel="noopener noreferrer"
                                                                 className="btn btn-primary btn-learn">View Portfolio <i className="icon-briefcase3" /></a></p>
                                                         </nav>
                                                     </div>
